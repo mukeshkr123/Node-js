994116ca914d9dded0bf4bd9553fd1fa2611852e
Mukesh Kumar <mkmehta2041@gmail.com>
2023-09-03 01:06:35 +0530

    Storing Data in Files Via the Model

--- shop/models/product.js
+++ shop/models/product.js
@@ -1,3 +1,7 @@
+const { error } = require("console");
+const fs = require("fs");
+const path = require("path");
+
 // Create an empty array to store product objects
 const products = [];
 
@@ -10,11 +14,36 @@ module.exports = class Product {
 
   // Method to save the product to the products array
   save() {
-    products.push(this); // Add the current product instance to the products array
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (error, fileContent) => {
+      let products = [];
+      if (!error) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   // Static method to fetch all products (no need to create an instance)
   static fetchAll() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
     return products; // Return the array of all products
   }
 };
